0d41b88ef3d22994e6bdd2993bfdd502c24f8e4b
Laz <lazgerasimov@gmail.com>
2022-12-04 21:44:08 +0000

    update guard

--- src/app/user/user-detail.guard.ts
+++ src/app/user/user-detail.guard.ts
@@ -1,18 +1,20 @@
 import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from "@angular/router";
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from "@angular/router";
 
 
 @Injectable({
     providedIn: 'root'
 })
-
+ 
 export class AuthGuard implements CanActivate{ 
+
+    constructor(private router: Router) {}
 
     canActivate(
         route: ActivatedRouteSnapshot, 
-        state: RouterStateSnapshot): boolean {
+        state: RouterStateSnapshot){
         
-            return this.checkIfLogged(state.url);
+            return this.checkIfLogged(state.url) || this.router.createUrlTree(['/user/list']);
     }
 
     checkIfLogged(url: string): boolean {
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { UserListComponet } from './list/list.component';
 import { UserDetailComponent } from './detail/detail.component';
 import { RouterModule } from '@angular/router';
+import { AuthGuard } from './user-detail.guard';
 
 
 
@@ -21,6 +22,7 @@ import { RouterModule } from '@angular/router';
       },
       {
         path: 'user/details/:id',
+        canActivate: [AuthGuard],
         component: UserDetailComponent
       },
     ])
