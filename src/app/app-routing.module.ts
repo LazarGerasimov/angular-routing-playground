196f8566ce3845365e081e4f7f74f6d5bb0edffa
Laz <lazgerasimov@gmail.com>
2022-12-04 18:41:34 +0000

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { PostListComponent } from './post/post-list/post-list.component';
 import { UserListComponet } from './user/list/list.component';
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { UserModule } from './user/user.module';
+import { PostModule } from './post/post.module';
+
 
 @NgModule({
   declarations: [
@@ -18,7 +20,8 @@ import { UserModule } from './user/user.module';
     AppRoutingModule,
     BrowserAnimationsModule,
     UserModule,
-    HttpClientModule
+    HttpClientModule,
+    PostModule
   ],
   providers: [],
   bootstrap: [AppComponent]
