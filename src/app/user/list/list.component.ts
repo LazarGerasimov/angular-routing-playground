e7507f2e89bdf4deaacf4a985d3729c49c3bbf5d
Laz <lazgerasimov@gmail.com>
2022-12-04 16:06:41 +0000

    add services to list

--- src/app/user/list/list.component.ts
+++ src/app/user/list/list.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { GlobalLoaderService } from 'src/app/core/services/global-loader.service';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
-export class ListComponent {
+export class ListComponent implements OnInit {
 
+  constructor(
+    private userService: UserService,
+    private globalLoader: GlobalLoaderService
+  ) { }
+
+  ngOnInit(): void {
+
+  }
 }
