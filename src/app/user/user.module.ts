9267b3f578e8482a214cab1814107dafd57400bf
Laz <lazgerasimov@gmail.com>
2022-12-04 19:27:50 +0000

    routing settings

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UserListComponet } from './list/list.component';
+import { DetailComponent } from './detail/detail.component';
 
 
 
 @NgModule({
   declarations: [
-    UserListComponet
+    UserListComponet,
+    DetailComponent
   ],
   imports: [
     CommonModule
